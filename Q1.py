93bd24a0cf1627074fe7c736c558776d85a65854
i-officialbharat <officialbharat100@gmail.com>
2026-07-25 20:28:32 +0530

    python module 1
    code with harry

---
+++ Q1.py
@@ -0,0 +1,6 @@
+# Q [1] WRITE A PROGRAM TO PRINT JONNEY JONNEY YES PAPA IN PYTHON.
+print ("""Johny, Johny,Yes, Papa?
+       Eating sugar?No, Papa!
+       Telling lies?No, Papa!
+       Open your mouth,
+       Ha, ha, ha!""")
