35e3d88b15179ee8d54b1aa20a5de441f21313fd
i-officialbharat <officialbharat100@gmail.com>
2026-07-28 22:43:40 +0530

    Add files via upload

---
+++ Q2{MOD 2 CODE WITH HARRY}.py
@@ -0,0 +1,11 @@
+
+# what is remainder when a number is devided by b
+a=50
+b=50
+print("remainder when a is divided by b is" , a%b)
+
+# or we can do
+a=int(input("enter the number 1,"))
+b=int(input("enter the number 2,"))
+
+print ("remainder when a is divided by b" ,a%b)
