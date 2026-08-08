80bad1eb37699b235a421313d7fb5add757970b0
i-officialbharat <officialbharat100@gmail.com>
2026-08-08 14:31:59 +0530

    Add files via upload

---
+++ Q5{MOD 2 CODE WITH HARRY}.py
@@ -0,0 +1,5 @@
+#Q 5. write a program to find average of two number enter by user
+a=int(input("enter the number 1, "))
+b=int(input("enter the number 2, "))
+
+print("the average of the two number is ",(a+b)/2)
