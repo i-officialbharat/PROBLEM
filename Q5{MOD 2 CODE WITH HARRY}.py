57a57b738e32abb00be05c13ee77cfb35344ef57
i-officialbharat <officialbharat100@gmail.com>
2026-08-11 20:30:08 +0530

    Delete Q5{MOD 2 CODE WITH HARRY}.py

--- Q5{MOD 2 CODE WITH HARRY}.py
+++
@@ -1,5 +0,0 @@
-#Q 5. write a program to find average of two number enter by user
-a=int(input("enter the number 1, "))
-b=int(input("enter the number 2, "))
-
-print("the average of the two number is ",(a+b)/2)
