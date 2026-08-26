44d19b84d3df6fb77ac182e9739c67b78753489f
i-officialbharat <officialbharat100@gmail.com>
2026-08-26 23:18:43 +0530

    Add files via upload

---
+++ Q6{MOD 2 CODE WITH HARRY}.py
@@ -0,0 +1,5 @@
+# Q 6. square of the number entered 
+a=int(input("enter the number 1, "))
+print("square of the number entered",a**2)
+# or print("square of the number entered",a*a)
+# we use * not ^
